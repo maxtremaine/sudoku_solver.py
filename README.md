927a88d3c9d128da1a24c3324ef4f2cd9418ef18
Max Tremaine <max.tremaine@gmail.com>
2020-06-07 14:05:30 -0400

    Added Cell class.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Solves Sudoku puzzles so I don't have to.
 ## To-Do
 - [x] Make the initializer generic.
 - [x] Make the generator take file input.
-- [ ] Create a Cell class.
+- [x] Create a Cell class.
 - [ ] Create a freedom generation method and add to instantiation.
 - [ ] Create a sorting mechanism by freedom.
+- [ ] Create a filtering mechanism by blanks
--- __main__.py
+++ __main__.py
@@ -5,23 +5,19 @@ with open("input/start.sudoku") as f:
     data = f.read()
 
 start_condition = Puzzle.create_from_file(data)
-working_conditions = [start_condition]
-index = 0
-sort_map = {}
-run_dict = [x for x in start_condition.__dict__.items()]
+run_dict = [x[1] for x in start_condition.__dict__.items()]
+working_conditions = [ start_condition ]
 
-for c, value in run_dict:
-    new_working_conditions = []
+# for key, cell in run_dict:
+#     new_working_conditions = []
 
-    if value == "_":
-        for working_condition in working_conditions:
-            for n in range(1, 10):
-                new_condition = shallow_copy(working_condition)
-                new_condition.__dict__[c] = n
-                if new_condition.check_puzzle():
-                    new_working_conditions.append(new_condition)
+#     if cell.value == "_":
+#         for working_condition in working_conditions:
+#             for n in range(1, 10):
+#                 new_condition = shallow_copy(working_condition)
+#                 new_condition.__dict__[key].value = n
+#                 if new_condition.check_puzzle():
+#                     new_working_conditions.append(new_condition)
 
-        print(len(new_working_conditions))
-        working_conditions = new_working_conditions
-
-print(working_conditions[0].__dict__)
+#         print(len(new_working_conditions))
+#         working_conditions = new_working_conditions
