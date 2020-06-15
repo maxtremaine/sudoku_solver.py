629193e94a7424ecee5686a83a2f721371d647e0
Max Tremaine <max.tremaine@gmail.com>
2020-06-15 00:40:01 -0400

    Added types.

--- __main__.py
+++ __main__.py
@@ -11,11 +11,11 @@ with open("data/start.sudoku") as f:
 if not start_condition.check_puzzle():
     raise Exception("The start condition is not valid.")
 
-working_conditions = [ start_condition ]
-run_sequence = start_condition.prioritize_blanks()
+working_conditions: List[Puzzle] = [ start_condition ]
+run_sequence: List[str] = start_condition.prioritize_blanks()
 
 def try_cases(test_case: Tuple[str, Puzzle]) -> List[Puzzle]:
-    passed_cases = []
+    passed_cases: List[Puzzle] = []
     for n in range(1, 10):
         new_condition = test_case[1].shallow_copy()
         new_condition.grid[Puzzle.data["cells"][test_case[0]]["grid_encoding"]] = str(n)
@@ -27,10 +27,10 @@ def try_cases(test_case: Tuple[str, Puzzle]) -> List[Puzzle]:
 for empty_cell_code in run_sequence:
     print(f"Running permutations on {empty_cell_code}.")
 
-    working_conditions = [ (empty_cell_code, x) for x in working_conditions ]
+    test_conditions = [ (empty_cell_code, x) for x in working_conditions ]
 
     with Pool() as p:
-        passed_case_lists: List[List[Puzzle]] = p.map(try_cases, working_conditions)
+        passed_case_lists: List[List[Puzzle]] = p.map(try_cases, test_conditions)
 
     new_working_conditions = [ x2 for x1 in passed_case_lists for x2 in x1 ]
     working_conditions = new_working_conditions
