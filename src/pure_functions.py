337ba36463f7e9c7a856c159fca08b63c3c5d8f0
maxtremaine <max.tremaine@gmail.com>
2021-08-23 21:57:13 -0400

    Added replace_character function.

--- src/pure_functions.py
+++ src/pure_functions.py
@@ -5,3 +5,8 @@ def get_missing_digits(digits: List[str]) -> List[str]:
         str(digit) for digit in range(1, 10)
         if str(digit) not in digits
     ]
+
+def replace_character(string: str, index: int, new_character: str):
+    string_list = list(string)
+    string_list[index] = new_character
+    return ''.join(string_list)
--- test/test_pure_functions.py
+++ test/test_pure_functions.py
@@ -1,7 +1,11 @@
 import unittest
-from src.pure_functions import get_missing_digits
+from src.pure_functions import get_missing_digits, replace_character
 
 class TestPureFunctions(unittest.TestCase):
     def test_get_missing_digits(self):
         self.assertEqual(get_missing_digits(['1', '2', '3']), ['4', '5', '6', '7', '8', '9'])
         self.assertEqual(get_missing_digits(['a', '5', '6', '3']), ['1', '2', '4', '7', '8', '9'])
+
+    def test_replace_character(self):
+        self.assertEqual(replace_character('Oh hi there.', 4, 'o'), 'Oh ho there.')
+        self.assertEqual(replace_character('0123456789', 5, '2'), '0123426789')
