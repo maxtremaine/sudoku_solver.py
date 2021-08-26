f660e308f34197a2203ac93290273ab8a13c8f9f
maxtremaine <max.tremaine@gmail.com>
2021-08-26 11:06:20 -0400

    Removed unnecessary test.

--- test/test_puzzle_actions.py
+++ test/test_puzzle_actions.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from src.puzzle_actions import (sudoku_file_to_string, sudoku_string_to_file, is_valid_puzzle,
-    get_related_cell_indexes, get_cell_values, get_cell_index)
+    get_related_cell_indexes, get_cell_values)
 
 class TestPuzzleActions(TestCase):
     def test_sudoku_file_to_string(self):
@@ -49,6 +49,3 @@ class TestPuzzleActions(TestCase):
     def test_get_cell_values(self):
         valid_puzzle = '7___4___1__1___2___6_2_9_8___35_49__1_______4__21_85___1_9_6_7___8___4__6___2___8'
         self.assertEqual(get_cell_values(valid_puzzle, [0, 2, 4]), ['4', '7'])
-
-    def test_get_cell_index(self):
-        self.assertEqual(get_cell_index(1), 'b1')
