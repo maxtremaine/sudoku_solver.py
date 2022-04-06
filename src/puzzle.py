3709bde6300f91d690306070cff3a74fd0fdcf9f
maxtremaine <max.tremaine@gmail.com>
2022-04-06 13:43:31 -0700

    Moved global functions into a Sudoku class.

---
+++ src/puzzle.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import List
+from src.puzzle_data import file_to_string_conversion_indexes, groups
+
+@dataclass(frozen=True)
+class Sudoku:
+    values: List[int]
+
+    @classmethod
+    def from_sudoku_file(cls, file_string: str):
+        def character_to_number(character):
+            if character == '_':
+                return 0
+            return int(character)
+
+        character_list = [ file_string[x] for x in file_to_string_conversion_indexes ]
+        number_list = [ character_to_number(x)  for x in character_list ]
+        return cls(number_list)
+
+    def is_valid(self) -> bool:
+        for group in groups:
+            values = [ 
+                self.values[index]
+                for index in group
+                if self.values[index] != 0
+            ]
+            if len(values) != len(set(values)):
+                return False
+        return True
+
+    @staticmethod
+    def get_related_cell_indexes():
+        pass
