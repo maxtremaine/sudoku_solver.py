e7300f1d75ee0ce5d6666d0d288ae0b62b5c6d75
Max Tremaine <max.tremaine@gmail.com>
2023-11-08 14:12:17 -0500

    Removed unnecessary call to str

--- __main__.py
+++ __main__.py
@@ -27,10 +27,10 @@ for run_count in range(1, start_puzzle.values.count(0) + 1):
     print(f'- {len(new_branches)} branches on run {run_count}.')
     working_branches = new_branches
 
-output = str(working_branches[0])
+output = working_branches[0]
 
 print(f'\nWOOO, we did it:\n{output}\n')
 print(f"Ran successfully in {datetime.now() - t0}.")
 
 with open('io/finish.sudoku', 'w') as f:
-    f.write(output)
+    f.write(str(output))
